5626ddbf3ed5ccce1f00c824a577764aeb3fbe4e
Bgenerowicz <basgen@wlan-145-94-232-66.wlan.tudelft.nl>
2017-05-24 10:05:00 +0200

    Added audio files + first main

---
+++ main.py
@@ -0,0 +1,66 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import soundfile as sf
+# import math
+
+#Variables
+tsegment = 20e-3 #20ms segment
+overlap = 0.5
+
+#Import data & fs
+data, fs = sf.read('Audio/clean.wav')
+
+#Calc
+s_segment = int(tsegment*fs)
+s_overlap = int(overlap*s_segment)
+
+
+#pad data with zeros
+remainder = s_segment - (len(data) % s_segment)
+data_extended = np.ravel(np.asmatrix(np.pad(data,(0,int(remainder)),'constant')))
+
+#calculate number of frames
+num_frames = int((len(data_extended)-s_overlap)/s_overlap)
+
+#init matrix
+x = np.matrix(np.zeros([num_frames,s_segment]))
+for i in range(0,num_frames):
+    x[i, :] = data_extended[i*s_overlap:i*s_overlap+s_segment]
+
+#transform back into array
+x_array = np.ravel(x)
+
+
+
+#########
+#Reverse#
+#########
+
+new_frames = int(len(data_extended)/s_segment)
+#init matrix
+x_trunc = np.matrix(np.zeros([new_frames,s_segment]))
+for j in range(0,new_frames):
+    x_trunc[j, :] = x_array[j*(2*s_segment):j*(2*s_segment)+s_segment]
+
+#transform back into array
+x_truncarray = np.ravel(x_trunc)
+
+#calculate difference between initial and reconstructed signals
+residual = data_extended - x_truncarray
+
+
+#Plots
+f, axarr = plt.subplots(3, sharex=True)
+axarr[2].plot(residual)
+axarr[2].set_title('Residual')
+axarr[1].plot(x_truncarray)
+axarr[1].set_title('Reconstructed')
+axarr[0].plot(data_extended)
+axarr[0].set_title('Original')
+
+
+end = 1
+
+
+
+
